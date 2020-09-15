73c06fee99bcdb88bbb8c4de54dbaec8892d44cd
Gabriela Possari <gabrielapossari@hotmail.com>
2020-09-15 16:58:36 -0300

    post funcionando empresa

---
+++ API/GoVagas/Controllers/UsuarioControllercs.cs
@@ -0,0 +1,103 @@
+﻿using GoVagas.Domains;
+using GoVagas.Interfaces;
+using GoVagas.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoVagas.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _UsuarioRepository;
+
+        public UsuarioController()
+        {
+            _UsuarioRepository = new UsuarioRepository();
+        }
+
+        /// <summary>
+        ///  Lista os Usuarios
+        /// </summary>
+        /// <returns>Retorna uma lisa de Usuarios</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_UsuarioRepository.ListarTodos());
+        }
+        /// <summary>
+        /// Lista um Usuario passando seu respectivo ID na URL
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Retorna um Status Code 200</returns>
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return StatusCode(200, _UsuarioRepository.BuscarPorId(id));
+        }
+        /// <summary>
+        /// Cadastra um novo Usuario
+        /// </summary>
+        /// <param name="novoUsuario"></param>
+        /// <returns> Status Code 201</returns>
+        [HttpPost]
+        public IActionResult Post(Usuario novoUsuario)
+        {
+            _UsuarioRepository.Cadastrar(novoUsuario);
+
+            return StatusCode(201);
+        }
+        /// <summary>
+        /// Atualiza os Usuarios passando o ID pela URL
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="UsuarioAtualizado"></param>
+        /// <returns> Status Cdoe 404</returns>
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Usuario UsuarioAtualizado)
+        {
+            Usuario UsuarioBuscado = _UsuarioRepository.BuscarPorId(id);
+
+            if (UsuarioBuscado != null)
+            {
+                try
+                {
+                    _UsuarioRepository.Atualizar(id, UsuarioAtualizado);
+
+                    return StatusCode(200);
+                }
+                catch (Exception erro)
+                {
+                    return BadRequest(erro);
+                }
+            }
+
+            return StatusCode(404);
+        }
+        /// <summary>
+        /// Deleta um usuário passando seu respectivo ID pela solução
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Stattus Code 200</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Usuario UsuarioBuscado = _UsuarioRepository.BuscarPorId(id);
+
+            if (UsuarioBuscado == null)
+            {
+                return NotFound();
+            }
+
+            _UsuarioRepository.Deletar(id);
+
+            return StatusCode(202);
+        }
+    }
+}
---
+++ API/GoVagas/Repositories/EmpresaRepository.cs
@@ -0,0 +1,112 @@
+﻿using GoVagas.Contexts;
+using GoVagas.Domains;
+using GoVagas.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoVagas.Repositories
+{
+    public class EmpresaRepository : IImpresaRepository
+    {
+        GoVagasContext ctx = new GoVagasContext();
+
+        public void Atualizar(int id, Empresa EmpresaAtualizado)
+        {
+            // Busca um evento através do id
+            Empresa empresaBuscada = ctx.Empresa.Find(id);
+
+            // Verifica se a empresa foi encontrada
+            if (empresaBuscada.AnexarLogo != null)
+            {
+                // Verifica se foi informado um novo AnexarLogo para a empresa
+                if (EmpresaAtualizado.AnexarLogo != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.AnexarLogo = EmpresaAtualizado.AnexarLogo;
+                }
+                // Verifica se foi informado um novo CargoArea para a empresa
+                if (EmpresaAtualizado.CargoArea != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.CargoArea = EmpresaAtualizado.CargoArea;
+                }
+
+                // Verifica se foi informada uma novo WebSite para a empresa
+                if (EmpresaAtualizado.WebSite != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.WebSite = EmpresaAtualizado.WebSite;
+                }
+
+                // Verifica se foi informada uma novo novo de empresa para a empresa
+                if (EmpresaAtualizado.NomeEmpresa != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.NomeEmpresa = EmpresaAtualizado.NomeEmpresa;
+                }
+
+                // Verifica se o cnpj foi informado
+                if (EmpresaAtualizado.Cnpj != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.Cnpj = EmpresaAtualizado.Cnpj;
+                }
+
+                // Verifica se a instituição foi informada
+                if (EmpresaAtualizado.RamoEmpresa != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.RamoEmpresa = EmpresaAtualizado.RamoEmpresa;
+                }
+                // Verifica se a descricao da empresa foi informada
+                if (EmpresaAtualizado.DescricaoEmpresa != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.DescricaoEmpresa = EmpresaAtualizado.DescricaoEmpresa;
+                }
+                // Verifica se a localizacao da foi informada
+                if (EmpresaAtualizado.LocalizacaoEmpresa != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.LocalizacaoEmpresa = EmpresaAtualizado.LocalizacaoEmpresa;
+                }
+                // Verifica se o campo encontrou senai foi informada
+                if (EmpresaAtualizado.EncontrouSenai != null)
+                {
+                    // Atribui o novo valor ao campo
+                    empresaBuscada.EncontrouSenai = EmpresaAtualizado.EncontrouSenai;
+                }
+                ctx.SaveChanges();
+            }
+        }
+
+
+        public Empresa BuscarPorId(int id)
+        {
+            return ctx.Empresa.FirstOrDefault(ca => ca.IdEmpresa == id);
+        }
+
+        public void Cadastrar(Empresa novoEmpresa)
+        {
+            // Adiciona um nova nova empresa
+            ctx.Empresa.Add(novoEmpresa);
+
+            // Salva as informações para serem gravadas no banco
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Empresa empresaBuscada = ctx.Empresa.Find(id);
+            ctx.Empresa.Remove(empresaBuscada);
+            ctx.SaveChanges();
+        }
+
+        public List<Empresa> ListarTodos()
+        {
+            return ctx.Empresa.ToList();
+        }
+    }
+}
